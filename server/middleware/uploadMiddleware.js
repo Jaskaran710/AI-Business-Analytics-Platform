ae0c329231ceb616116ffa39f38920214087489b
Jaskaran Arora <jaskaran07.arora@gmail.com>
2026-06-03 03:53:43 +0530

    Fix uploads directory creation on Render

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,17 +1,34 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, {
+    recursive: true
+  });
+}
 
 const storage = multer.diskStorage({
 
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+
+    cb(null, uploadDir);
+
   },
 
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+
+    cb(
+      null,
+      Date.now() + "-" + file.originalname
+    );
+
+  }
 
 });
 
-const upload = multer({ storage });
-
-module.exports = upload;
+module.exports = multer({
+  storage
+});
